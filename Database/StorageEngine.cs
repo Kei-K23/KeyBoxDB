e22ff13ec617f478f5da3fd2d7f2f5dfce29744b
Kei-K23 <keiksl2301@gmail.com>
2024-11-17 18:06:45 +0630

    feat: implement simple storage engine class to handle to save data to database file and load data from database file

---
+++ Database/StorageEngine.cs
@@ -0,0 +1,33 @@
+using System.Collections.Concurrent;
+using System.Text.Json;
+using KeyBoxDB.Models;
+
+namespace KeyBoxDB.Database
+{
+    public class StorageEngine(string filePath)
+    {
+        private string _filePath = filePath;
+
+        public void Save(ConcurrentDictionary<string, Record> data)
+        {
+            // Convert JSON object into JSON string
+            var jsonData = JsonSerializer.Serialize(data);
+            // Write to file
+            File.WriteAllText(_filePath, jsonData);
+        }
+
+        public ConcurrentDictionary<string, Record> Load()
+        {
+            var emptyData = new ConcurrentDictionary<string, Record>();
+            // Not found database file, then return new empty record
+            if (!File.Exists(_filePath))
+            {
+                return emptyData;
+            }
+
+            // Read JSON string data from database file and convert to JSON object
+            var jsonStrData = File.ReadAllText(_filePath);
+            return JsonSerializer.Deserialize<ConcurrentDictionary<string, Record>>(jsonStrData) ?? emptyData;
+        }
+    }
+}
